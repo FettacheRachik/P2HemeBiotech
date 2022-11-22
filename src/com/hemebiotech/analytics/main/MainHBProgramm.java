fc71c556f762ead7ec6e89118beb1168f184356e
Fettache Rachik <racfet@gmail.com>
2022-11-22 12:08:41 +0100

    Refactor and make differents packages with interfaces and classes

---
+++ src/com/hemebiotech/analytics/main/MainHBProgramm.java
@@ -0,0 +1,10 @@
+package com.hemebiotech.analytics.main;
+
+public class MainHBProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
