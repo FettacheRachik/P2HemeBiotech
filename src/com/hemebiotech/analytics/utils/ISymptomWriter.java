3117a931da09795ed935a0666986d3d2d202420a
Fettache Rachik <racfet@gmail.com>
2022-11-24 11:12:26 +0100

    Version final

--- src/com/hemebiotech/analytics/utils/ISymptomWriter.java
+++ src/com/hemebiotech/analytics/utils/ISymptomWriter.java
@@ -1,14 +1,14 @@
 package com.hemebiotech.analytics.utils;
 
 import java.util.Map;
+
 /**
- * will write symptom data which are order and count 
+ * will write symptom data which are order and count
  */
 public interface ISymptomWriter {
 
-	
 	/**
-	 * 
+	 * Method to write symptoms in output.txt
 	 * @param Map<String,Integer> symptomes order and count each of them
 	 */
 	public void writeSymptoms(Map<String, Integer> symptomes);
